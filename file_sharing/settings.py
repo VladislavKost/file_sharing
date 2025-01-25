0e199791a7b99acaf09e83f4479e1d7a4ebbc78e
Vladislav Kostrov <vladislav.kostrov@rydlab.ru>
2025-01-25 13:19:42 +0500

    [Fix] Update setting.py

--- file_sharing/settings.py
+++ file_sharing/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 import environ
+import os
 from pathlib import Path
 from datetime import timedelta
 from corsheaders.defaults import default_headers
@@ -191,6 +192,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
